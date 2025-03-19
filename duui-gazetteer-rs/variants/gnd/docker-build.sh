526a7af650d81dc9a4fa94498da8fe043383a0e1
Manuel Schaaf <manuel.schaaf@em.uni-frankfurt.de>
2025-03-19 11:19:18 +0100

    minor(gazetteer): Updated docker build scripts

--- duui-gazetteer-rs/variants/gnd/docker-build.sh
+++ duui-gazetteer-rs/variants/gnd/docker-build.sh
@@ -1,4 +1,5 @@
-export VERSION=1.5.4
+export VERSION=1.6.1
+export GAZETTER_VERSION=1.6.1
 export NAME=gnd
 
 rsync -ahz --info=Progress2 sirao:/resources/nlp/gazetteer_lists/filter_de.txt resources/
@@ -7,7 +8,7 @@ if [ ! -f resources/Kern-Personennamen.lex.gazetteer.v2.json.txt.gz ]; then
   pigz resources/Kern-Personennamen.lex.gazetteer.v2.json.txt || gzip resources/Kern-Personennamen.lex.gazetteer.v2.json.txt;
 fi;
 
-docker build --build-arg VERSION="${VERSION}" -t gazetteer-rs/${NAME}:${VERSION} .
+docker build --build-arg VERSION="${GAZETTER_VERSION}" -t gazetteer-rs/${NAME}:${VERSION} .
 docker image tag gazetteer-rs/${NAME}:${VERSION} docker.texttechnologylab.org/gazetteer-rs/${NAME}:latest
 docker image tag gazetteer-rs/${NAME}:${VERSION} docker.texttechnologylab.org/gazetteer-rs/${NAME}:${VERSION}
 docker push docker.texttechnologylab.org/gazetteer-rs/${NAME}:latest
